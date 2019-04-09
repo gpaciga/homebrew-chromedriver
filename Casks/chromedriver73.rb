3e8dfcf61b1080a1f5f96b65b5a1581d7bc3ab1c
Gregory Paciga <gpaciga@gmail.com>
2019-04-09 12:01:46 -0400

    initial stab at separate casks for chromedriver versions 73 and 74

---
+++ Casks/chromedriver73.rb
@@ -0,0 +1,13 @@
+cask 'chromedriver73' do
+  version '73.0.3683.68'
+  sha256 'eaaa1b0b7d47b113d228ca99a5d68de52f660ccd9dd78a069df8cd97ff83308a'
+
+  # chromedriver.storage.googleapis.com was verified as official when first introduced to the cask
+  url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip"
+  appcast 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
+  name 'ChromeDriver'
+  homepage 'https://sites.google.com/a/chromium.org/chromedriver/home'
+
+  binary 'chromedriver'
+end
+
