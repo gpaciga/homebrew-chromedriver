f4ff8c95775abbc2e57286e5731adf90f77ee33a
Gregory Paciga <gpaciga@gmail.com>
2019-11-14 16:40:16 -0500

    add v78

---
+++ Casks/chromedriver78.rb
@@ -0,0 +1,15 @@
+cask 'chromedriver78' do
+  version '78.0.3904.70'
+  sha256 'f04501f36af92ab869111137a66c84612e69ec729caa29b25fa4c305614cfe37'
+
+  # chromedriver.storage.googleapis.com was verified as official when first introduced to the cask
+  url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip"
+  appcast 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
+  name 'ChromeDriver'
+  homepage 'https://sites.google.com/a/chromium.org/chromedriver/home'
+
+  conflicts_with cask: 'chromedriver-beta'
+
+  binary 'chromedriver'
+end
+
