f1c0dc3895053a21e754d99dd1ef32448d3be6c1
Gregory Paciga <gpaciga@gmail.com>
2020-01-08 17:00:53 -0500

    add v79

---
+++ Casks/chromedriver79.rb
@@ -0,0 +1,14 @@
+cask 'chromedriver79' do
+  version '79.0.3945.36'
+  sha256 'c3bbd1139ace81268fe6d2e74fc815a2fc86d95b3c617eca4a70c92d7cba1b1c'
+
+  # chromedriver.storage.googleapis.com was verified as official when first introduced to the cask
+  url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip"
+  appcast 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
+  name 'ChromeDriver'
+  homepage 'https://sites.google.com/a/chromium.org/chromedriver/home'
+
+  conflicts_with cask: 'chromedriver-beta'
+
+  binary 'chromedriver'
+end
